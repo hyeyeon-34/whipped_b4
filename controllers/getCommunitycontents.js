36be0d4b020cf2d4519a71d373ef8a02b52c04f3
hyeyeon-34 <hyee092@gmail.com>
2024-09-19 17:48:26 +0900

    TEST3

--- controllers/getCommunitycontents.js
+++ controllers/getCommunitycontents.js
@@ -1,10 +1,10 @@
 const database = require('../database/database');
 
 exports.getAnnouncement = async (req, res) => {
-    const announcement = req.params.announcement;
+
   
     try {
-      const result = await database.query('SELECT * FROM product where write_number=$1', [announcement]);
+      const result = await database.query('SELECT * FROM community');
       return res.status(200).json(result.rows);
     } catch (error) {
       return res.status(500).json({ msg: 'Get Items Fail' + error });
